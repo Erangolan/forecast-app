d5cc83feed3d4444e97ea2da9363a216eb7d4458
Erangolan <37651948+Erangolan@users.noreply.github.com>
2022-04-01 13:16:41 +0300

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    allowImportExportEverywhere: true,
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'quotes': ['error', 'single'],
+    'indent': ['error', 2],
+    'no-multi-spaces': ['error'],
+    'no-trailing-spaces': ['error', { 'skipBlankLines': false }],
+    'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 0 }],
+    'no-extra-semi': ['error'],
+    semi: ['error', 'never'],
+  },
+}
